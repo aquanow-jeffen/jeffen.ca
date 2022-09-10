6f3e1fc2c922b1415637eea2e727e04b8de3a1f6
Jeffen Chen <jeffen@aquanow.io>
2022-09-10 13:45:40 -0700

    feat: add new writing

--- src/pages/writing.tsx
+++ src/pages/writing.tsx
@@ -2,13 +2,18 @@ import type { NextPage } from "next";
 import PageLayout from "@components/pageLayout";
 import { Box } from "@mui/system";
 import { WritingCard } from "@modules/writing/writingCard";
-import Divider from "@components/Divider";
 
 const writingData = [
   {
     title: "Event Driven Architecture and Event Sourcing",
     subtitle: "A research on how event sourcing looks like on AWS",
     url: "https://blog.jeffen.me/event-driven-architecture-and-event-sourcing-d2d78982e523?source=friends_link&sk=03f0cf86d41539e63f9b9d7cafc34180",
+  },
+  {
+    title: "NodeJS Lambda Best Practices with Serverless Framework",
+    subtitle:
+      "An opinionated guide on how to organize a serverless application",
+    url: "https://blog.jeffen.me/nodejs-lambda-best-practices-with-serverless-framework-c0c185960353",
   },
 ];
 
